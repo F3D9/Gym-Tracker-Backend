3faafb9ff733b50634f0a3e7d4c6160a0bb034e2
Federico Salgado <federico.salgado04@gmail.com>
2026-07-31 19:12:27 +0000

    habilito cors de github

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['https://f3d9.github.io', 'http://localhost:5173'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({transform: true}));
